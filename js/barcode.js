88b919c68ed4c4adc52f0ee30c9803f3771757c7
Johan Ferreras <ferrerasjohan@gmail.com>
2014-09-09 18:41:00 -0400

    bs

--- js/barcode.js
+++ js/barcode.js
@@ -10,10 +10,17 @@ function startScan() {
 
 	cordova.plugins.barcodeScanner.scan(
 		function (result) {
-			var s = "Result: " + result.text + "<br/>" +
-			"Format: " + result.format + "<br/>" +
-			"Cancelled: " + result.cancelled;
-			resultDiv.innerHTML = s;
+			// var s = "Result: " + result.text + "<br/>" +
+			// "Format: " + result.format + "<br/>" +
+			// "Cancelled: " + result.cancelled;
+			// resultDiv.innerHTML = s;
+			  jQuery.getJSON("http://isbndb.com/api/v2/json/9CDSTNTF/book/"+result.text, function(api_result) {
+                var book = api_result["data"][0];
+                $("#title").val(book["title_long"]);
+                $("#author").val(book["author_data"][0]["name"]);
+                $("isbn").val(book["isbn13"]);
+            });
+
 		}, 
 		function (error) {
 			alert("Scanning failed: " + error);
